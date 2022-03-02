69a915a390300cd600fe2a49f462388022c1ba06
Aisha Marmash <aishamarmash@gmail.com>
2022-03-02 11:54:36 +0200

    adding a new class as user Input reader

---
+++ Project Management Application/ProjectsUserInputReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Management_Application
+{
+    internal class ProjectsUserInputReader
+    {
+        public static string[] GetSearchInput()
+        {
+            string[] listOfChoices = { "Project Name", "Task Name", "Task Status", "Assigned Contributor" };
+            Console.WriteLine($"Search on");
+            string[] UserInput = new string[4];
+            int counter = 0;
+            do
+            {
+                Console.WriteLine($"** {listOfChoices[counter]}: ");
+                UserInput[counter] = Console.ReadLine();
+                Console.WriteLine(UserInput[counter]);
+                counter++;
+            }
+            while (counter < listOfChoices.Length);
+            return UserInput;
+        }
+    }
+}
